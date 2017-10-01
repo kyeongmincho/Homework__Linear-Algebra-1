8cb2ea80855516a949c01db2ffe26f98d20b2430
drzix <korea.drzix@gmail.com>
2017-10-01 20:23:43 +0900

    matrix: to_reduced_row_echelon_form() 최적화 및 주석 작성

--- lib/matrix.py
+++ lib/matrix.py
@@ -1,24 +1,26 @@
 from fractions import Fraction
 
 
-class Matrix:
-    """
-    행렬을 나타내는 클래스.
-    """
+class SquareMatrix:
+    """정방행렬을 나타내는 클래스."""
 
     def __init__(self, rows):
-        """
-        행렬 클래스의 initiator.
+        """정방행렬 클래스의 initiator.
+        행렬의 원소를 세팅해준다.
 
         :param rows: 행렬의 각각의 원소인 integer 혹은 Fraction들로 구성된
                      행렬의 각각의 행인 tuple이나 list들로 구성된
                      tuple이나 list가 들어와야 한다.
                      tuple로 구성된 list, tuple로 구성된 tuple, list로 구성된 list, list로 구성된 tuple
                      어떤 것도 상관 없다.
+                     (단, 1x1 행렬의 경우 tuple이 생략되어 버리니 list로 해야함.)
 
-        ex) my_mtrx = Matrix(((1, 0, 1231231, 3),
-                              (2, 3, 4, 3241),
-                              (-1, -3, -3, -4)))
+        Examples
+        --------
+
+        my_mtrx = SquareMatrix(((1, 0, 1231231, 3),
+                                (2, 3, 4, 3241),
+                                (-1, -3, -3, -4)))
         """
         if type(rows) is not list and type(rows) is not tuple:
             raise Exception('matrix param must be list or tuple')
@@ -49,26 +51,28 @@ class Matrix:
 
         self.nr_row = len(self.rows)
 
+        if self.nr_row + 1 != self.nr_column:
+            raise Exception('not a square matrix')
+
     def to_reduced_row_echelon_form(self):
+        """행렬을 reduced row echelon form 형태로 바꾸어준다.
+        해가 무한하거나 없는 경우에는 가능한 만큼 1과 0 으로 원소들을 바꾼다.
+        먼저 가능한 기준행을 찾은 뒤 다른 행에 적절한 곱을 한 형태를 합하는 방법으로 기준 열의 값을 0으로 만들어준다.
+        """
         pending_col_idx = 0
         for i in range(0, self.nr_row):
-            if not self._can_be_pivot(row_idx=i, col_idx=pending_col_idx):
-                avail_pivot_idx = self._find_available_pivot(row_idx_upper_bound=i, col_idx=pending_col_idx)
-                if avail_pivot_idx is None:
-                    continue
-                self._swap_rows(i, avail_pivot_idx)
+            pivot_row_idx, pivot_col_idx = self._find_available_pivot(row_idx_lower_bound=i,
+                                                                      col_idx_lower_bound=pending_col_idx)
+            if pivot_row_idx is None:
+                break
 
-            self._make_one_row_echelon_form(row_idx=i, col_idx=pending_col_idx)
-            self._add_other_rows_from_echelon_form_row(echelon_form_row_idx=i, pivot_col_idx=pending_col_idx)
-            pending_col_idx += 1
-
-            self.print()
-            print("----------------------------------")
+            self._swap_rows(i, pivot_row_idx)
+            self._make_one_row_echelon_form(row_idx=i, col_idx=pivot_col_idx)
+            self._add_other_rows_from_echelon_form_row(echelon_form_row_idx=i, pivot_col_idx=pivot_col_idx)
+            pending_col_idx = pivot_col_idx + 1
 
     def print(self):
-        """
-        행렬 전체를 출력한다.
-        """
+        """행렬 전체를 출력한다."""
         form_str = '{:>' + str(self._get_max_len_elem_str()) + 's}'
 
         for i in range(0, self.nr_row):
@@ -78,6 +82,10 @@ class Matrix:
             print('')
 
     def _get_max_len_elem_str(self):
+        """행렬 안의 원소들 중 가장 긴 문자열의 길이를 구한다.
+
+        :return: 행렬 안의 원소들 중 가장 긴 문자열의 길이
+        """
         max_len = 0
         for i in range(0, self.nr_row):
             for j in range(0, self.nr_column):
@@ -88,11 +96,21 @@ class Matrix:
         return max_len
 
     def _make_one_row_echelon_form(self, row_idx, col_idx):
+        """한 행을 기준행으로 만들기 위해 기준열의 원소값을 1로 바꾸기 위해 곱하는 작업을 한다.
+
+        :param row_idx: 기준행이 될 행의 인덱스
+        :param col_idx: 값이 1이 될 원소의 열 인덱스
+        """
         rev = Fraction(self.rows[row_idx][col_idx].denominator, self.rows[row_idx][col_idx].numerator)
         for i in range(col_idx, self.nr_column):
             self.rows[row_idx][i] *= rev
 
     def _add_other_rows_from_echelon_form_row(self, echelon_form_row_idx, pivot_col_idx):
+        """기준행을 이용하여 다른 행들에 적절한 합을 해서 기준열의 원소값을 0으로 만들어준다.
+
+        :param echelon_form_row_idx: 기준행의 인덱스
+        :param pivot_col_idx: 0이 될 원소의 열 인덱스
+        """
         pivot_row = self.rows[echelon_form_row_idx]
         for r_i in range(0, self.nr_row):
             if r_i == echelon_form_row_idx:
@@ -102,14 +120,34 @@ class Matrix:
             for c_i in range(pivot_col_idx, self.nr_column):
                 self.rows[r_i][c_i] += pivot_row[c_i] * rev_coefficient
 
-    def _can_be_pivot(self, row_idx, col_idx):
-        return self.rows[row_idx][col_idx] != 0
+    def _find_available_pivot(self, row_idx_lower_bound, col_idx_lower_bound):
+        """기준행이 가질 수 있는 가장 왼쪽의 미지수를 찾고 그 행과 열의 인덱스를 구한다.
+        이미 기준행이 된 적이 있는 행들을 제외한 행들 중에서 0이 아닌 값을 갖는 원소의 열 인덱스가 가장 작은 행을 찾는다.
+        WARNING: 가장 오른쪽 열의 원소들은 미지수가 아니므로 참조하지 않는다.
 
-    def _find_available_pivot(self, row_idx_upper_bound, col_idx):
-        for i in range(row_idx_upper_bound + 1, self.nr_row):
-            if self.rows[i][col_idx] != 0:
-                return i
-        return None
+        :param row_idx_lower_bound: 기준행이 된 적이 없는 행들 중 가장 작은 인덱스
+        :param col_idx_lower_bound: 기준열이 된 적이 없는 열들 중 가장 작은 인덱스
+        :return: 다음으로 기준행이 될 행의 인덱스와 그 행에서 1이 될 열의 인덱스
+        """
+        min_row_idx = None
+        min_col_idx = None
+        for i in range(row_idx_lower_bound, self.nr_row):
+            for j in range(col_idx_lower_bound, self.nr_column - 1):
+                if self.rows[i][j] != 0:
+                    if min_col_idx is None or j < min_col_idx:
+                        min_row_idx = i
+                        min_col_idx = j
+                        break
+            if min_col_idx == col_idx_lower_bound:
+                return min_row_idx, min_col_idx
+
+        return min_row_idx, min_col_idx
 
     def _swap_rows(self, first_idx, second_idx):
+        """행과 행의 순서를 교체한다.
+        두 개의 파라미터는 같을 수도 있다 (변화가 없음)
+
+        :param first_idx: 바꿀 행 중 하나
+        :param second_idx: 바꿀 행 중 다른 하나
+        """
         self.rows[second_idx], self.rows[first_idx] = self.rows[first_idx], self.rows[second_idx]
--- run.py
+++ run.py
@@ -1,14 +1,24 @@
 from lib.matrix import *
 
 try:
-    # a = Matrix(((1, 2, -1, -4),
-    #             (2, 3, -1, -12),
-    #             (-2, 0, -3, 2)))
-    a = Matrix(((12, 4, -2, 17),
-                (1, 2, 3, 14),
-                (-3, -4, 5, -2)))
-    a.to_reduced_row_echelon_form()
-    a.print()
+    # sm = SquareMatrix([[8, 5]])  # 1x1
+    # sm = SquareMatrix([[2, 3, 10],  # 2x2
+    #                   [6, 10, -4]])
+    sm = SquareMatrix([[1, 2, -1, -4],  # 3x3
+                      [2, 3, -1, -12],
+                      [-2, 0, -3, 2]])
+    # sm = SquareMatrix([[1, 2, -1, 1, -4],  # 4x4
+    #                   [1, 7, 5, 4, -12],
+    #                   [1, 2, 5, 6, -12],
+    #                   [1, 2, 2, 7, -12]])
+    # sm = SquareMatrix([[1, 2, -1, 1, 3, -4],  # 5x5
+    #                   [6, 2, 23, 4, 12, 89182734981723],
+    #                   [7, 6, 2, 8, 12, -2],
+    #                   [8, 5, -9, 7, 66, -12],
+    #                   [15, 2, -34, 1, 4, -86]])
+
+    sm.to_reduced_row_echelon_form()
+    sm.print()
 
 except Exception as e:
     print('Error: ' + e.args[0])
