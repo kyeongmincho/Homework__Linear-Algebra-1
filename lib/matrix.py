abb8778e8f8fee982aeaefc96c4377cd50541fbe
drzix <korea.drzix@gmail.com>
2017-10-01 18:49:42 +0900

    matrix: to_reduced_row_echelon_form() 구현

--- lib/matrix.py
+++ lib/matrix.py
@@ -10,7 +10,7 @@ class Matrix:
         """
         행렬 클래스의 initiator.
 
-        :param rows: 행렬의 각각의 원소인 integer를로 구성된
+        :param rows: 행렬의 각각의 원소인 integer 혹은 Fraction들로 구성된
                      행렬의 각각의 행인 tuple이나 list들로 구성된
                      tuple이나 list가 들어와야 한다.
                      tuple로 구성된 list, tuple로 구성된 tuple, list로 구성된 list, list로 구성된 tuple
@@ -46,30 +46,70 @@ class Matrix:
                     new_elem = Fraction(c)
 
                 self.rows[-1].append(new_elem)
-                len_new_elem = len(str(new_elem.numerator) + '/' + str(new_elem.denominator))
-                self.nr_max_width = len_new_elem \
-                    if self.nr_max_width is None or len_new_elem > self.nr_max_width \
-                    else self.nr_max_width
 
         self.nr_row = len(self.rows)
+
+    def to_reduced_row_echelon_form(self):
+        pending_col_idx = 0
+        for i in range(0, self.nr_row):
+            if not self._can_be_pivot(row_idx=i, col_idx=pending_col_idx):
+                avail_pivot_idx = self._find_available_pivot(row_idx_upper_bound=i, col_idx=pending_col_idx)
+                if avail_pivot_idx is None:
+                    continue
+                self._swap_rows(i, avail_pivot_idx)
+
+            self._make_one_row_echelon_form(row_idx=i, col_idx=pending_col_idx)
+            self._add_other_rows_from_echelon_form_row(echelon_form_row_idx=i, pivot_col_idx=pending_col_idx)
+            pending_col_idx += 1
+
+            self.print()
+            print("----------------------------------")
 
     def print(self):
         """
         행렬 전체를 출력한다.
         """
+        form_str = '{:>' + str(self._get_max_len_elem_str()) + 's}'
 
-        form_str = '{:' + str(self.nr_max_width) + 's}'
-
-        for r in self.rows:
-            for c in r:
-                if c.denominator == 1:
-                    printed_str = str(c.numerator)
-                else:
-                    printed_str = str(c.numerator) + '/' + str(c.denominator)
-
-                print(form_str.format(printed_str), end='  ')
+        for i in range(0, self.nr_row):
+            for j in range(0, self.nr_column):
+                print(form_str.format(str(self.rows[i][j])), end='  ')
 
             print('')
 
-    def to_reduced_row_echelon_form(self):
-        return
+    def _get_max_len_elem_str(self):
+        max_len = 0
+        for i in range(0, self.nr_row):
+            for j in range(0, self.nr_column):
+                cur_len = len(str(self.rows[i][j]))
+                if cur_len > max_len:
+                    max_len = cur_len
+
+        return max_len
+
+    def _make_one_row_echelon_form(self, row_idx, col_idx):
+        rev = Fraction(self.rows[row_idx][col_idx].denominator, self.rows[row_idx][col_idx].numerator)
+        for i in range(col_idx, self.nr_column):
+            self.rows[row_idx][i] *= rev
+
+    def _add_other_rows_from_echelon_form_row(self, echelon_form_row_idx, pivot_col_idx):
+        pivot_row = self.rows[echelon_form_row_idx]
+        for r_i in range(0, self.nr_row):
+            if r_i == echelon_form_row_idx:
+                continue
+
+            rev_coefficient = -self.rows[r_i][pivot_col_idx]
+            for c_i in range(pivot_col_idx, self.nr_column):
+                self.rows[r_i][c_i] += pivot_row[c_i] * rev_coefficient
+
+    def _can_be_pivot(self, row_idx, col_idx):
+        return self.rows[row_idx][col_idx] != 0
+
+    def _find_available_pivot(self, row_idx_upper_bound, col_idx):
+        for i in range(row_idx_upper_bound + 1, self.nr_row):
+            if self.rows[i][col_idx] != 0:
+                return i
+        return None
+
+    def _swap_rows(self, first_idx, second_idx):
+        self.rows[second_idx], self.rows[first_idx] = self.rows[first_idx], self.rows[second_idx]
--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from lib.matrix import *
 
 try:
-    a = Matrix(((1, 0, 12323231, 3),
-                (2, 3, 4, 3241),
-                (-1, -3, -3, -4)))
+    # a = Matrix(((1, 2, -1, -4),
+    #             (2, 3, -1, -12),
+    #             (-2, 0, -3, 2)))
+    a = Matrix(((12, 4, -2, 17),
+                (1, 2, 3, 14),
+                (-3, -4, 5, -2)))
+    a.to_reduced_row_echelon_form()
     a.print()
+
 except Exception as e:
     print('Error: ' + e.args[0])
